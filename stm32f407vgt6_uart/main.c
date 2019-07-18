8f775a5c33ecdf3889186032d64df5c5a6d2bb23
Diego Hinojosa <diego@manglar.cl>
2019-07-18 17:07:41 -0400

    Implement stdio printf using uart2

--- stm32f407vgt6_uart/main.c
+++ stm32f407vgt6_uart/main.c
@@ -8,27 +8,81 @@
 
 
 #include "stm32f4xx.h"                  // Device header
+#include <stdio.h>
 
 void USART2_Init(void);
-void USART2_write(int ch);
+int USART2_write(int ch);
 char USART2_read(void);
 void delayMs(int delay);
 void LED_play(int value);
+
+// Wrapping to use printf
+struct __FILE
+{
+	int handle;
+};
+
+FILE __stdin  = {0};
+FILE __stdout = {1};
+FILE __stderr = {2};
+
+int fgetc(FILE *f)
+{
+	int c;
+	c = USART2_read();
+	/*
+	if(c == '\r')
+	{
+		USART2_write(c);
+		c='\n';
+	}
+	USART2_write(c);
+	*/
+	
+	return c;
+}
+
+int fputc(int c, FILE *f)
+{
+	return USART2_write(c);
+}
+
+// end wrapping printf
 
 int main(void)
 {
 	RCC->AHB1ENR |= (1<<3); 			// Enable clock to PortD
 	GPIOD->MODER |= 0x40000000; 	// Set pin PD15 as output
 	
-	GPIOD->BSRR = 0x8000; 				//Turn on LED PD15 
+	GPIOD->BSRR = 0x8000; 				// Turn on LED PD15 
 	delayMs(100);
-	GPIOD->BSRR = 0x80000000; 		//Turn off LED PD15 
+	GPIOD->BSRR = 0x80000000; 		// Turn off LED PD15 
 	
-	USART2_Init();
+	USART2_Init();								// Init USART2
 	
-	char ch;
+	char ch;	
+	
+	int n;
+	char str[100];
+	
+	printf("Hello world printf\r\n");
+	fprintf(stdout, "test for stdout\r\n");
+	fprintf(stderr, "test for stderr\r\n");
 	
 	while(1)
+	{
+		printf("How old are you?\r\n");
+		scanf("%d",&n);
+		printf("You age is: %d\r\n",n);
+		printf("Enter your first name:");
+		gets(str);
+		printf("I like your style ");
+		puts(str);
+		printf("\r\n");
+	}
+	
+	/*
+	while(1)	
 	{
 		ch = USART2_read();
 		USART2_write(ch);
@@ -38,6 +92,7 @@ int main(void)
 			LED_play(ch);
 		}
 	}
+	*/
 
 	return 0;
 }
@@ -47,6 +102,7 @@ void USART2_Init(void)
 {
 	// enable USART2 clock, bit 17 on APB1ENR
 	RCC->APB1ENR |= (1 << 17);
+	
 	// enable GPIOA clock, bit 0 on AHB1ENR
 	RCC->AHB1ENR |= (1 << 0);
 	
@@ -61,14 +117,11 @@ void USART2_Init(void)
 	GPIOA->AFR[0] |= (0x7 << 8); 	// for pin 2
 	GPIOA->AFR[0] |= (0x7 << 12); // for pin 3
 
-	// usart2 tx enable, TE bit 3
-	USART2->CR1 |= (1 << 3);
+	
+	USART2->CR1 |= (1 << 3);	// usart2 tx enable, TE bit 3
+	USART2->CR1 |= (1 << 2);	// usart2 rx enable, RE bit 2
 
-	// usart2 rx enable, RE bit 2
-	USART2->CR1 |= (1 << 2);
-
-
-	USART2->BRR = 0x008B;  //115200 baudrate @16Mhz
+	USART2->BRR = 0x008B;  		//115200 baudrate @16Mhz
 
 	// enable usart2 - UE, bit 13
 	USART2->CR1 |= (1 << 13);
@@ -80,15 +133,16 @@ char USART2_read(void)
 	return USART2->DR;
 }
 
-void USART2_write(int ch)
+int USART2_write(int ch)
 {
 	while(!(USART2->SR & 0x0080)){} // wait while TX buffer is empty
 	USART2->DR = (ch & 0xFF);
+	return ch;
 }
 
 void LED_play(int value)
 {
-	GPIOD->BSRR = 0x8000; //Turn LED on
+	GPIOD->BSRR = 0x8000; 		//Turn LED on
 	delayMs(10);
 	GPIOD->BSRR = 0x80000000; //Turn LED off
 	delayMs(10);
